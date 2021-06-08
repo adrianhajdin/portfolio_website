a5d41f89dd4a0fa61c2c85c01bce20d0ef1bb91d
Adrian Hajdin - JavaScript Mastery <adrianhajdin00@gmail.com>
2021-06-08 18:53:31 +0200

    remove title

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -32,7 +32,6 @@ export default class MyDocument extends Document {
       <Html lang='en-GB'>
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
-          <title>Portfolio</title>
         </Head>
         <body>
           <Main />
