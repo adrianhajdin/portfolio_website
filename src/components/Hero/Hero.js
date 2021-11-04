af8ba31353856cd57b363db9fdca4c5906172465
Mezo <dominik@koch-bautechnik.de>
2021-11-04 23:25:55 +0100

    Fixed button not working
    Button simply wasnt working so here is the fix. Also opens the site in a new tab if you dont want that remove the ''_blank"

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -15,7 +15,7 @@ const Hero = (props) => (
         <SectionText>
         The purpose of JavaScript Mastery is to help aspiring and established developers to take their development skills to the next level and build awesome apps.
         </SectionText>
-        <Button onClick={props.handleClick}>Learn More</Button>
+        <Button onClick={() => window.open('https://google.com', '_blank')}>Learn More</Button>
       </LeftSection>
     </Section>
   </>
