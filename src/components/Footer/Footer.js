31b126529f9d3b889cff793c3cd15146151994e7
Debjit Biswas <debjit012@gmail.com>
2021-12-31 23:23:09 +0530

    update footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,12 +10,12 @@ const Footer = () => {
       <LinkList>
         <LinkColumn>
           <LinkTitle>Call</LinkTitle>
-          <LinkItem href="tel:314-343-3432">314-343-3432</LinkItem>
+          <LinkItem href="tel:0000000000">0000000000</LinkItem>
         </LinkColumn>
         <LinkColumn>
           <LinkTitle>Email</LinkTitle>
-          <LinkItem href="mailto:contact@jsmastery.com">
-            contact@jsmastery.com
+          <LinkItem href="mailto:hi@debjit.in">
+          hi@debjit.in
           </LinkItem>
         </LinkColumn>
       </LinkList>
