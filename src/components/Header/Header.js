27367dad329cece16d6e57e27aff5ac344a3318b
Debjit Biswas <debjit012@gmail.com>
2021-12-31 23:38:33 +0530

    WIP

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,7 +10,7 @@ const Header = () =>  (
     <Div1>
       <Link href="/">
         <a style={{ display: 'flex', alignItems: 'center', color:"white" }}>
-          <DiCssdeck size="3rem" /> <span>Portfolio</span>
+          <DiCssdeck size="3rem" /> <span>Debjit's Portfolio</span>
         </a>
       </Link>
     </Div1>
