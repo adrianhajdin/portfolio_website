441cbf3ea9f562bd8a73a4016a39c66ab43b6e33
Debjit Biswas <debjit012@gmail.com>
2022-01-03 15:11:44 +0530

    1

--- functions/hello.js
+++ functions/hello.js
@@ -1,5 +1,5 @@
 exports.handler = async event => {
-    const subject = event.queryStringParameters.name || 'World'
+    const subject = event.queryStringParameters.name || 'World +1'
     return {
         statusCode: 200,
         body: `Hello ${subject}!`,
