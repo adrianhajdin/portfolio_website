3804651ed470d989c58226449b2e9240f7e4dd7c
Niels van Gortel <niels.vangortel@student.hu.nl>
2022-04-05 20:51:06 +0200

    finish

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,35 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+   <FooterWrapper>
+     <LinkList>
+      <LinkColumn>
+        <LinkTitle>Call</LinkTitle>
+        <LinkItem href="Tel: 06-573-208-14">06-573-208-14</LinkItem>
+      </LinkColumn>
+      <LinkColumn>
+        <LinkTitle>Email</LinkTitle>
+        <LinkItem href="mailto: nielsvangortel@gmail.com">nielsvangortel@gmail.com</LinkItem>
+      </LinkColumn>
+     </LinkList>
+     <SocialIconsContainer>
+       <CompanyContainer>
+         <Slogan>There is no dresscode here</Slogan>
+       </CompanyContainer>
+       <SocialContainer>
+       <SocialIcons href="https://github.com">
+        <AiFillGithub size="3rem"/>
+      </SocialIcons>
+    
+      <SocialIcons href="https://linkedin.com">
+        <AiFillLinkedin size="3rem"/>
+      </SocialIcons>
+      <SocialIcons href="https://instagram.com">
+        <AiFillInstagram size="3rem"/>
+      </SocialIcons>
+      </SocialContainer>
+     </SocialIconsContainer>
+   </FooterWrapper>
   );
 };
 
--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -4,9 +4,47 @@ import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id='tech'>
+    <SectionDivider />
+    <br />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I've worked with variants of technologies like JavaScript, React, Xcode, Java, Backend and Frontend
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="5rem" />
+        <ListContainer>
+        <ListParagraph>
+          <ListTitle>Front-End</ListTitle>
+            Experiance with <br />
+            React.js
+        </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="5rem" />
+        <ListContainer>
+        <ListParagraph>
+          <ListTitle>Back-End</ListTitle>
+            Experiance with <br />
+            Json 
+        </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiZend size="5rem" />
+        <ListContainer>
+        <ListParagraph>
+          <ListTitle>UX/UI</ListTitle>
+            Experiance with <br />
+            Wireframes 
+        </ListParagraph>
+        </ListContainer>
+      </ListItem>
+     
+    </List>
+  </Section>
 );
 
 export default Technologies;
