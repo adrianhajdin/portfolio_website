35ecf695408f6c466bc57cfd62e4820794b2c40b
Niels van Gortel <niels.vangortel@student.hu.nl>
2022-04-05 20:56:05 +0200

    finish

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -39,8 +39,8 @@ export const projects = [
 
 export const TimeLineData = [
   { year: 2020, text: 'Started my journey', },
-  { year: 2018, text: 'Worked as a freelance developer', },
-  { year: 2019, text: 'Founded JavaScript Mastery', },
-  { year: 2020, text: 'Shared my projects with the world', },
-  { year: 2021, text: 'Started my own platform', },
+  { year: 2018, text: 'Started a developer education', },
+  { year: 2019, text: 'Founded JavaScript', },
+  { year: 2020, text: 'Shared my projects with the world', }
+
 ];
