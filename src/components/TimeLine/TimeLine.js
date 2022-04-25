be2f2c62083431dc391afa5c930106ca01fd9785
christopherdan1 <65066462+christopherdan1@users.noreply.github.com>
2022-04-25 09:56:26 -0400

    Update TimeLine.js

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -67,9 +67,8 @@ const Timeline = () => {
     <Section id="about">
       <SectionTitle>About Me</SectionTitle>
       <SectionText>
-        The purpose of JavaScript Mastery is to help aspiring and established
-        developers to take their development skills to the next level and build
-        awesome apps.
+        The purpose of HolderDesigns is to bring valuable design and development solutions
+        to assist with modern business problems.
       </SectionText>
       <CarouselContainer ref={carouselRef} onScroll={handleScroll}>
         <>
